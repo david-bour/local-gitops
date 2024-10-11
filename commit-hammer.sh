eaf985d75f032e39acbc5a42a3d021432a411601
david bour <dbour.developer@gmail.com>
2024-10-10 23:01:14 -0700

    Changed date

--- commit-hammer.sh
+++ commit-hammer.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-yq -i ".dateTime = \"$(date)\"" ./argocd-applications/environments/dev/values.yaml
-git add .
-git commit -m "Changed date"
-git push
+
+while true; do
+    yq -i ".dateTime = \"$(date)\"" ./argocd-applications/environments/dev/values.yaml
+    git add .
+    git commit -m "Changed date"
+    git push
+    sleep 2
+done
