56f74437c203b2a1c0b9b197338959f1b51f8346
david bour <dbour.developer@gmail.com>
2024-10-28 11:06:15 -0700

    give me 200

--- modify-app-count.py
+++ modify-app-count.py
@@ -1,11 +1,13 @@
 import yaml
 
+APP_COUNT=200
+
 def generate_apps(n):
     return [{'index': x + 1} for x in range(n)]
 
 with open("./argocd-applications/applicationsets/nginx.applicationset.yaml") as fi:
     yaml_fi = yaml.safe_load(fi)
-    yaml_fi['spec']['generators'][0]['list']['elements'] = generate_apps(50)
+    yaml_fi['spec']['generators'][0]['list']['elements'] = generate_apps(APP_COUNT)
 
 with open('./argocd-applications/applicationsets/nginx.applicationset.yaml', 'w') as fi_out:
     yaml.dump(yaml_fi, fi_out)
