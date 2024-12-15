af293c8bb43796d0729a331d79f1341803a241bb
therealdavidbour <therealdavidbour>
2024-12-14 20:34:20 -0800

    newapp

--- echo-app/app/main.py
+++ echo-app/app/main.py
@@ -1,7 +1,9 @@
 import os
 import time
 import logging
+import requests
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 logger = logging.getLogger(__name__)
 
@@ -14,7 +16,22 @@ app = FastAPI()
 def read_root():
     return {"status": "healthy ❤️"}
 
+@app.get("/info")
+def info():
+    return {"name": os.environ.get("APPNAME", "No Name")}
+
 @app.get('/{echo}')
 def echo(echo: str):
     return {"msg": echo}
+
+class Proxy(BaseModel):
+    url: str
+
+@app.post('/proxy')
+def proxy(data: Proxy):
+    results = requests.get(data.url)
+    try:
+        return results.json()
+    except Exception:
+        return results.text
 
